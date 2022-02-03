ff3fc1e7bb19a67936606e99124eeaad5b4dfec3
Serg <serg@example.com>
2022-02-03 23:53:36 +0300

    added life-cycle demo

---
+++ sfg-di-09-life-cycle-demo/src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+	}
+
+}
